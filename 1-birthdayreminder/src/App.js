0eb5fd366c05ebe0cb42a35185a321bdd5d8547f
NgTuanLoc <18521011@gm.uit.edu.vn>
2021-04-02 15:50:05 +0700

    finish birthday reminder project

--- 1-birthdayreminder/src/App.js
+++ 1-birthdayreminder/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+import List from "./components/List";
+import { data } from "./data/data";
+
+const App = () => {
+  const [people, setPeople] = useState(data);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <main>
+      <section className="container">
+        <h3>{people.length} Birthdays today</h3>
+        <List people={people} />
+        <button
+          onClick={() => {
+            setPeople([]);
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          Clear All !
+        </button>
+      </section>
+    </main>
   );
-}
+};
 
 export default App;
