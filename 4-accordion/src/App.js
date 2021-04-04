58e85d15d7e92915461ae30e4a8d6c9df68263d9
NgTuanLoc <18521011@gm.uit.edu.vn>
2021-04-04 16:15:58 +0700

    project 4: accordion completed

--- 4-accordion/src/App.js
+++ 4-accordion/src/App.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
+import data from "./data/data";
+import SingleQuestion from "./components/Question";
 
 function App() {
-  return null;
+  const [questions, setQuestions] = useState(data);
+  return (
+    <main>
+      <div className="container">
+        <h3>Question and Answer about login</h3>
+        <section className="info">
+          {questions.map((question)=>{
+            return <SingleQuestion key={question.key} {...question}/>
+          })}
+        </section>
+      </div>
+    </main>
+  )
 }
 
 export default App;
--- 4-accordion/src/components/Question.js
+++ 4-accordion/src/components/Question.js
@@ -1,7 +1,19 @@
-import React from 'react'
+import React, { useState } from "react";
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
 
-const Question = ()=>{
-    return <h2>Question Component</h2>
-}
+const Question = ({ title, info }) => {
+  const [showInfo, setShowInfo] = useState(true);
+  return (
+    <article className="question">
+      <header>
+        <h4>{title}</h4>
+        <button className="btn" onClick={() => setShowInfo(!showInfo)}>
+          {showInfo ? <AiOutlineMinus /> : <AiOutlinePlus />}
+        </button>
+      </header>
+      {showInfo && <p>{info}</p>}
+    </article>
+  );
+};
 
-export default Question
+export default Question;
