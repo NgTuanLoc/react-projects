8f7a9493ea26f732444489c5a99b635638105a3e
NgTuanLoc <18521011@gm.uit.edu.vn>
2021-04-06 09:21:53 +0700

    setup project 5 MENU

---
+++ 5-menu/src/App.js
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+
+import Categories from "./components/Categories";
+import Menu from "./components/Menu";
+import data from "./data/data";
+
+function App() {
+  return <h2>Menu Setup</h2>;
+}
+
+export default App;
---
+++ 5-menu/src/components/Menu.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Menu = () => {
+  return <h2>Menu Components</h2>;
+};
+
+export default Menu;
