8d4d29ae70433c98a20996dbcd9192fa25f63f87
NgTuanLoc <18521011@gm.uit.edu.vn>
2021-07-30 15:21:43 +0700

    fix

--- 04-accordion/src/App.js
+++ 04-accordion/src/App.js
@@ -1,16 +1,15 @@
-import React, { useState } from "react";
+import React from "react";
 import data from "./data/data";
 import SingleQuestion from "./components/Question";
 
 function App() {
-  const [questions, setQuestions] = useState(data);
   return (
     <main>
       <div className="container">
         <h3>Question and Answer about login</h3>
         <section className="info">
-          {questions.map((question) => {
-            return <SingleQuestion key={question.key} {...question} />;
+          {data.map((item, index) => {
+            return <SingleQuestion key={index} {...item} />;
           })}
         </section>
       </div>
