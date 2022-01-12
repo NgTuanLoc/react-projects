93209e203d45f00aebc20a986b1e2c3db272c301
NgTuanLoc <18521011@gm.uit.edu.vn>
2022-01-12 13:55:10 +0700

    finsih project 19, setup project 20 darkmode

--- 19-photoApp/src/App.js
+++ 19-photoApp/src/App.js
@@ -1,74 +1,113 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { FaSearch } from "react-icons/fa";
 import Photo from "./Photo";
-import "./index.css";
 
 const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`;
 const mainUrl = `https://api.unsplash.com/photos/`;
 const searchUrl = `https://api.unsplash.com/search/photos/`;
 
+// remove current scroll code
+// set default page to 1
+// setup two useEffects
+// don't run second on initial render
+// check for query value
+// if page 1 fetch images
+// otherwise setPage(1)
+// fix scroll functionality
+
 const App = () => {
   const [loading, setLoading] = useState(false);
   const [photos, setPhotos] = useState([]);
-  const [page, setPage] = useState(1);
-
+  const [page, setPage] = useState(0);
+  const [query, setQuery] = useState("");
+  const mounted = useRef(false);
+  const [newImages, setNewImages] = useState(false);
   const fetchImages = async () => {
+    setLoading(true);
+    let url;
     const urlPage = `&page=${page}`;
-    let url = `${mainUrl}${clientID}${urlPage}`;
-
+    const urlQuery = `&query=${query}`;
+    if (query) {
+      url = `${searchUrl}${clientID}${urlPage}${urlQuery}`;
+    } else {
+      url = `${mainUrl}${clientID}${urlPage}`;
+    }
     try {
-      setLoading(true);
       const response = await fetch(url);
       const data = await response.json();
-      setPhotos((prevPhotos) => [...prevPhotos, ...data]);
+      setPhotos((oldPhotos) => {
+        if (query && page === 1) {
+          return data.results;
+        } else if (query) {
+          return [...oldPhotos, ...data.results];
+        } else {
+          return [...oldPhotos, ...data];
+        }
+      });
+      setNewImages(false);
       setLoading(false);
     } catch (error) {
+      setNewImages(false);
+
       setLoading(false);
-      console.log(error);
     }
   };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-  };
-
   useEffect(() => {
     fetchImages();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [page]);
 
   useEffect(() => {
-    window.addEventListener("scroll", () => {
-      if (
-        !loading &&
-        window.innerHeight + window.scrollY >= document.body.scrollHeight - 2
-      ) {
-        console.log("It works");
-        setPage((prevPage) => prevPage + 1);
-        console.log(page);
-      }
-    });
+    if (!mounted.current) {
+      mounted.current = true;
+      return;
+    }
+    if (!newImages) return;
+    if (loading) return;
+    setPage((oldPage) => oldPage + 1);
+  }, [loading, newImages]);
+
+  const event = () => {
+    if (window.innerHeight + window.scrollY >= document.body.scrollHeight - 2) {
+      setNewImages(true);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", event);
+    return () => window.removeEventListener("scroll", event);
   }, []);
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!query) return;
+    if (page === 1) {
+      fetchImages();
+    }
+    setPage(1);
+  };
   return (
     <main>
       <section className="search">
         <form className="search-form">
-          <input type="text" placeholder="search" className="form-input" />
-          <button type="submit" className="submit-btn" onClick={handleSubmit}>
+          <input
+            className="form-input"
+            placeholder="search"
+            type="text"
+            onChange={(e) => setQuery(e.target.value)}
+          />
+          <button className="submit-btn" onClick={handleSubmit}>
             <FaSearch />
           </button>
         </form>
       </section>
-
       <section className="photos">
         <div className="photos-center">
-          {photos &&
-            photos.map((photo) => {
-              return <Photo key={photo.id} {...photo} />;
-            })}
+          {photos.map((photo) => (
+            <Photo key={photo.id} {...photo} />
+          ))}
         </div>
-        {loading && <h2 className="loading">Loading ...</h2>}
+        {loading && <h2 className="loading">Loading ....</h2>}
       </section>
     </main>
   );
